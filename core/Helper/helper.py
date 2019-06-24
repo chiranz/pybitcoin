74ccd84877fabfd6b1f9de9a016155b7223bb28c
chiranz <chiranpoudyal@gmail.com>
2019-06-24 09:24:02 +0530

    Transactions TxFetcher, Tx, TxIn, TxOut

--- core/Helper/helper.py
+++ core/Helper/helper.py
@@ -73,3 +73,41 @@ def int_to_big_endian(n, length):
 
 def big_endian_to_int(b):
     return int.from_bytes(b, 'big')
+
+
+def read_variant(s):
+    '''reads a variable integer from a stream'''
+    i = s.read(1)[0]
+    if i == 0xfd:
+        # 0xfd means next two bytes are the number
+        return little_endian_to_int(s.read(2))
+
+    elif i == 0xfe:
+        # 0xfe means the next four bytes are the number
+        return little_endian_to_int(s.read(4))
+
+    elif i == 0xff:
+        # 0xff means the next eight bytes are the number
+        return little_endian_to_int(s.read(8))
+
+    else:
+        # Anything else is an integer
+        return i
+
+
+def encode_variant(i):
+    '''encodes an integer as a variant'''
+    if i < 0xfd:
+        return bytes([i])
+
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+
+    elif i < 0x100000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+
+    elif i < 0x10000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+
+    else:
+        raise ValueError(f"integer too large: {i}")
